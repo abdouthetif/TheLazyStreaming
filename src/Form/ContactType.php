76be2b1bf7cf8dda1a21f3078be5c68694b7dc25
Zahere Azali <zahere.azali@outlook.fr>
2021-01-25 12:34:36 +0100

    Contact Form

---
+++ src/Form/ContactType.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Form;
+
+use App\Controller\ContactController;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => 'true', 'label' => 'Nom de l\'utilisateur'
+            ])
+            ->add('email', EmailType::class, [
+                'required' => 'true', 'label' => 'Email de l\'utilisateur'
+            ])
+            ->add('objet', TextType::class, [
+                'required' => 'true', 'label' => 'Objet'
+            ])
+            ->add('message', TextareaType::class, [
+                'required' => 'true', 'label' => 'Message'
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data class' => ContactController::class,
+        ]);
+    }
+}
